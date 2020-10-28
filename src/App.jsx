8271750bb14688cb9f808a0c4ff5272c1900cd6d
dianecasanova <festusnova1989@gmail.com>
2020-10-28 16:59:53 +0100

    remove open burger

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import Navbar from './Components/Navbar';
-import Burger from './Components/Burger';
-import RightNav from './Components/RightNav';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Burger />
-      <RightNav />
       app
     </div>
   );
