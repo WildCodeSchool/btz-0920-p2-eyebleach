25fa3bc9bcc27dd01afa7669579e43b273277833
Malo Pivert <pivertmalo@gmail.com>
2020-10-28 15:34:46 +0100

    displaying img name favorite and download

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
+/* import { Component } from 'react';
+ */
 import './App.css';
+import PostPageMobile from './Components/postPageMobile';
+
+/* class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  PostPageDis() {
+    this.setState(() => {
+      OUIOUItrue; 
+    });  début d'une méthode qui permettrait de changer de page quand on clique sur l'image
+  }
+
+  render() {
+    return (
+      <h1>
+        {' '}
+        coucou
+        <PostPageMobile />
+      </h1>
+    );
+  }
+} */
 
 function App() {
   return (
-    <div className="App">
-      <h1> coucou</h1>
-    </div>
+    <body>
+      <h1>
+        {' '}
+        coucou
+        <PostPageMobile />
+      </h1>
+    </body>
   );
 }
 
---
+++ src/Components/postPageMobile.jsx
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+
+import PostPageMobilePhoto from './PostPageMobilePhoto';
+
+class PostPageMobile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      test: 'tout va bien',
+      name: 'Malo',
+    };
+  }
+
+  render() {
+    const { test, name } = this.state;
+    return (
+      <div>
+        <header>{/* Ici va le composant header de diane */}</header>
+        <h1>
+          oui ça fonctionne {name} {test}{' '}
+        </h1>
+        <PostPageMobilePhoto />
+      </div>
+    );
+  }
+}
+
+export default PostPageMobile;
