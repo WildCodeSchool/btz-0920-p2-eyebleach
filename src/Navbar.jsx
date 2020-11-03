c53d67b96c9f0f0a76ac2a3543a2ec1e2b5f94fa
dianecasanova <festusnova1989@gmail.com>
2020-11-03 11:06:20 +0100

    New Navbar with reactstrap

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,29 +1,79 @@
 import './App.css';
-import { Nav, Navbar } from 'react-bootstrap';
+import { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText,
+  Button,
+} from 'reactstrap';
 
-function App() {
+import PropTypes from 'prop-types';
+import logoEyebleach from './logoEyebleach.png';
+
+const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <div className="App">
-      <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">
+    <div>
+      <Navbar color="light" light expand="sm">
+        <NavbarBrand href="#home">
           <a href="/">
-            <img src=".src/logo-reddit.png" alt="logo" height="70" />
+            <img src={logoEyebleach} alt="logo" height="70" />
           </a>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#photos">Photos</Nav.Link>
-            <Nav.Link href="#Vidéos">Vidéos</Nav.Link>
-            <Nav.Link href="#Tops">Tops</Nav.Link>
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/Photos/">Photos</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/Vidéos/">Vidéos</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/Tops/">Tops</NavLink>
+            </NavItem>
           </Nav>
-          <Nav>
-            <Nav.Link button="#Reddit">View on Reddit</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+          <Button
+            color="danger"
+            href="https://www.reddit.com/r/Eyebleach/"
+            target="_blank"
+          >
+            View on Reddit
+          </Button>
+        </Collapse>
       </Navbar>
     </div>
   );
-}
+};
 
+Navbar.propTypes = {
+  light: PropTypes.bool,
+  dark: PropTypes.bool,
+  fixed: PropTypes.string,
+  color: PropTypes.string,
+  role: PropTypes.string,
+  expand: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+};
+
+NavbarBrand.propTypes = {
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+};
+
+NavbarText.propTypes = {
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+};
+
+NavbarToggler.propTypes = {
+  type: PropTypes.string,
+  tag: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+};
 export default App;
