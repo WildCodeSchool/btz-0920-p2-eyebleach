0684e164511e8f2d8d8be9ec915d4d86920b81f1
yoanmarcos <yomarcos@free.fr>
2020-11-06 09:07:26 +0100

    testing the render for the videos,not working

--- src/Components/PostPreview.jsx
+++ src/Components/PostPreview.jsx
@@ -1,31 +1,13 @@
-import {
-  Col,
-  Card,
-  CardTitle,
-  CardText,
-  CardImg,
-  CardImgOverlay,
-} from 'reactstrap';
+import { Col, Card, CardTitle, CardText, CardImgOverlay } from 'reactstrap';
 
 import PropTypes from 'prop-types';
 
-const PostPreview = ({
-  title,
-  url_overridden_by_dest,
-  author_fullname,
-  id,
-}) => {
-  const isImage = url_overridden_by_dest.split('.').pop() === `jpg`;
+const PostPreview = ({ title, url, author_fullname }) => {
+  // const isImage = url_overridden_by_dest.split('.').pop() === `jpg`;
   return (
     <Col xs="12" md="6" className="py-1">
       <Card inverse>
-        <p>{id}</p>
-        {isImage ? (
-          <CardImg width="100%" src={url_overridden_by_dest} alt={title} />
-        ) : (
-          <iframe title={title} src={url_overridden_by_dest} />
-        )}
-        )
+        <iframe title={title} src={url} />)
         <CardImgOverlay>
           <CardTitle tag="h5">{title}</CardTitle>
           <CardText>
@@ -39,9 +21,8 @@ const PostPreview = ({
 
 PostPreview.propTypes = {
   title: PropTypes.string.isRequired,
-  url_overridden_by_dest: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
   author_fullname: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
 };
 
 export default PostPreview;
