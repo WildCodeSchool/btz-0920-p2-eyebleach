3fddc80c66e953e196cf35ba85dbd1444121cc3d
yoanmarcos <yomarcos@free.fr>
2020-11-12 10:49:04 +0100

    create new file PostTop

--- src/Pages/TopPage.jsx
+++ src/Pages/TopPage.jsx
@@ -1,12 +1,50 @@
+import { useEffect, useState } from 'react';
+import Axios from 'axios';
 import { Row, Container } from 'reactstrap';
 
+import Loader from '../Components/Loader';
+import TextWelcome from '../Components/TextWelcome';
+import PostVideo from '../Components/PostVideo';
+
 const TopPage = () => {
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    Axios.get('https://www.reddit.com/r/Eyebleach.json').then((res) => {
+      setPosts(res.data.data.children);
+      setLoading(false);
+    });
+  }, []);
+
   return (
-    <Container>
-      <Row>
-        <p>UPVOTE !!!</p>
-      </Row>
-    </Container>
+    <div>
+      <Container>
+        <Row>
+          <TextWelcome />
+          {loading && <Loader />}
+
+          {posts &&
+            posts.map((post) => {
+              return (
+                <PostVideo
+                  id={post.data.permalink.split('/')[4]}
+                  slugTitle={post.data.permalink.split('/')[5]}
+                  title={post.data.title}
+                  // url_overridden_by_dest={post.data.url_overridden_by_dest}
+                  author_fullname={post.data.author}
+                  key={post.data.id}
+                  preview={
+                    // post.data.preview &&
+                    // post.data.preview.reddit_video_preview &&
+                    post.data.preview?.reddit_video_preview?.fallback_url
+                  }
+                />
+              );
+            })}
+        </Row>
+      </Container>
+    </div>
   );
 };
 
