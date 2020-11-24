d75f71ec52b003705c6c925f8df8e37f963bda90
yoanmarcos <yomarcos@free.fr>
2020-11-24 10:08:12 +0100

    create a filter who render only the Top Post using filter and sort array method

---
+++ src/Components/PostTop.jsx
@@ -0,0 +1,74 @@
+import { useHistory } from 'react-router-dom';
+import { Card, CardText, CardImg, CardImgOverlay } from 'reactstrap';
+
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import { VscAccount } from 'react-icons/vsc';
+import { BsHeart } from 'react-icons/bs';
+import { BiShareAlt } from 'react-icons/bi';
+
+import './PostPreview.css';
+
+const PostTop = ({
+  id,
+  slugTitle,
+  title,
+  url_overridden_by_dest,
+  author,
+  preview,
+  award,
+}) => {
+  const [isVideo, setIsVideo] = useState(false);
+  useEffect(() => {
+    if (preview) setIsVideo(preview.split('.').pop() === 'mp4');
+  }, [preview]);
+  const history = useHistory();
+
+  const goToPage = () => {
+    history.push(`/Post/${id}/${slugTitle}`);
+  };
+
+  return (
+    <Card
+      inverse
+      onClick={goToPage}
+      style={{
+        cursor: 'pointer',
+      }}
+    >
+      {isVideo && (
+        <video autoPlay="false" loop width="100%" src={preview}>
+          <track default kind="captions" />
+        </video>
+      )}
+      {!isVideo && (
+        <CardImg top width="100%" src={url_overridden_by_dest} alt={title} />
+      )}
+      <CardImgOverlay className="hideinfos">
+        <CardText className="w-100 d-flex justify-content-between">
+          <div>
+            <VscAccount className="mr-2" size={20} />
+            {author}
+          </div>
+          <div>
+            <BsHeart className="mr-2" size={20} />
+            <BiShareAlt size={20} />
+          </div>
+          <div>{award}</div>
+        </CardText>
+      </CardImgOverlay>
+    </Card>
+  );
+};
+
+PostTop.propTypes = {
+  title: PropTypes.string.isRequired,
+  url_overridden_by_dest: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  slugTitle: PropTypes.string.isRequired,
+  preview: PropTypes.objectOf(PropTypes.objectOf(PropTypes.string)).isRequired,
+  award: PropTypes.number.isRequired,
+};
+
+export default PostTop;
--- src/Pages/TopPage.jsx
+++ src/Pages/TopPage.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from 'react';
 import Axios from 'axios';
-import { Row, Container } from 'reactstrap';
+import { Row, Container, CardColumns } from 'reactstrap';
 
-import Loader from '../Components/Loader';
 import TextWelcome from '../Components/TextWelcome';
-import PostVideo from '../Components/PostVideo';
+import Loader from '../Components/Loader';
 
-const TopPage = () => {
+import './Home.css';
+import PostTop from '../Components/PostTop';
+
+const Home = () => {
   const [loading, setLoading] = useState(true);
   const [posts, setPosts] = useState([]);
 
@@ -18,34 +20,45 @@ const TopPage = () => {
   }, []);
 
   return (
-    <div>
-      <Container>
-        <Row>
-          <TextWelcome />
+    <Container>
+      <Row>
+        <TextWelcome className="text-responsive" />
+      </Row>
+      <Row className="d-flex justify-content-center">
+        <CardColumns className="cardscolumslayout px-3">
           {loading && <Loader />}
 
           {posts &&
-            posts.map((post) => {
-              return (
-                <PostVideo
-                  id={post.data.permalink.split('/')[4]}
-                  slugTitle={post.data.permalink.split('/')[5]}
-                  title={post.data.title}
-                  // url_overridden_by_dest={post.data.url_overridden_by_dest}
-                  author_fullname={post.data.author}
-                  key={post.data.id}
-                  preview={
-                    // post.data.preview &&
-                    // post.data.preview.reddit_video_preview &&
-                    post.data.preview?.reddit_video_preview?.fallback_url
-                  }
-                />
-              );
-            })}
-        </Row>
-      </Container>
-    </div>
+            posts
+              .filter((post) => post.data.total_awards_received >= 5)
+              .sort((a, b) => {
+                return (
+                  b.data.total_awards_received - a.data.total_awards_received
+                );
+              })
+              .map((post) => {
+                return (
+                  <PostTop
+                    id={post.data.permalink.split('/')[4]}
+                    slugTitle={post.data.permalink.split('/')[5]}
+                    title={post.data.title}
+                    url_overridden_by_dest={post.data.url_overridden_by_dest}
+                    author={post.data.author}
+                    key={post.data.id}
+                    award={post.data.total_awards_received}
+                    preview={
+                      // post.data.preview &&
+                      // post.data.preview.reddit_video_preview &&
+                      post.data.preview?.reddit_video_preview?.fallback_url
+                    }
+                  />
+                );
+              })
+              .slice(1, 50)}
+        </CardColumns>
+      </Row>
+    </Container>
   );
 };
 
-export default TopPage;
+export default Home;
