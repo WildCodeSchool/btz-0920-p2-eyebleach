5c6e1b0629e020ddedf6d9bcc9be0f451a1dee76
yoanmarcos <yomarcos@free.fr>
2020-11-24 16:43:36 +0100

    delete css line in PostVideo

--- src/Components/PostVideo.jsx
+++ src/Components/PostVideo.jsx
@@ -7,8 +7,6 @@ import { BiShareAlt } from 'react-icons/bi';
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 
-import './PostVideo.css';
-
 const PostVideo = ({ id, slugTitle, author, preview }) => {
   const [isVideo, setIsVideo] = useState(false);
   useEffect(() => {
@@ -26,7 +24,6 @@ const PostVideo = ({ id, slugTitle, author, preview }) => {
   return (
     <Card
       inverse
-      className="d-flex justify-content-center"
       onClick={goToPage}
       style={{
         cursor: 'pointer',
@@ -37,6 +34,7 @@ const PostVideo = ({ id, slugTitle, author, preview }) => {
           <track default kind="captions" />
         </video>
       )}
+
       <CardImgOverlay className="hideinfos">
         <CardText className="w-100 d-flex justify-content-between">
           <div>
